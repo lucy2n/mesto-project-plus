a9311ea904195271a6ed3763fa0363d0e65f4c99
lucy2n <lyusianaumenko@yandex.by>
2023-12-02 15:12:03 +0300

    fix: fix remarks 2

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import userRouter from './routes/users';
 import cardRouter from './routes/cards';
 import { IUserRequest } from './types';
+import { NOT_FOUND } from './utils/constants';
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -24,6 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
+
+app.use('*', (req, res) => {
+  res.status(NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
